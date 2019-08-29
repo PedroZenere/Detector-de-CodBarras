acea35e8013a62042f70ef53deaf77188fb58df5
PedroZenere <pedrinhozenere99@gmail.com>
2019-08-29 15:35:35 -0500

    Ta funcionando com webcam, porem ele so capta um frame e para

--- detect_barcode.py
+++ detect_barcode.py
@@ -1,60 +1,89 @@
 # import the necessary packages
-from pyimagesearch import simple_barcode_detection
+
 from imutils.video import VideoStream
 import argparse
 import time
 import cv2
+import numpy as np
+import imutils
+
+def detect(image):
+	# convert the image to grayscale
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
  
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-args = vars(ap.parse_args())
+	# compute the Scharr gradient magnitude representation of the images
+	# in both the x and y direction using OpenCV 2.4
+	ddepth = cv2.cv.CV_32F if imutils.is_cv2() else cv2.CV_32F
+	gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
+	gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
  
-# if the video path was not supplied, grab the reference to the
-# camera
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
+	# subtract the y-gradient from the x-gradient
+	gradient = cv2.subtract(gradX, gradY)
+	gradient = cv2.convertScaleAbs(gradient)
  
-# otherwise, load the video
-else:
-	vs = cv2.VideoCapture(args["video"])
-# keep looping over the frames
-while True:
-	# grab the current frame and then handle if the frame is returned
-	# from either the 'VideoCapture' or 'VideoStream' object,
-	# respectively
-	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
+	# blur and threshold the image
+	blurred = cv2.blur(gradient, (9, 9))
+	(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
  
-	# check to see if we have reached the end of the
-	# video
-	if frame is None:
-		break
+	# construct a closing kernel and apply it to the thresholded image
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+	closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
  
-	# detect the barcode in the image
-	box = simple_barcode_detection.detect(frame)
+	# perform a series of erosions and dilations
+	closed = cv2.erode(closed, None, iterations=4)
+	closed = cv2.dilate(closed, None, iterations=4)
  
-	# if a barcode was found, draw a bounding box on the frame
-	if box is not None:
-		cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+	# find the contours in the thresholded image
+	cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
  
-	# show the frame and record if the user presses a key
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+	# if no contours were found, return None
+	if len(cnts) == 0:
+		return None
  
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+	# otherwise, sort the contours by area and compute the rotated
+	# bounding box of the largest contour
+	c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+	rect = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+	box = np.int0(box)
  
-# if we are not using a video file, stop the video file stream
-if not args.get("video", False):
-	vs.stop()
- 
-# otherwise, release the camera pointer
-else:
-	vs.release()
- 
-# close all windows
-cv2.destroyAllWindows()
+	# return the bounding box of the barcode
+	return box
+	
+
+
+def main():
+	cap = cv2.VideoCapture(0)
+
+	# Check if the webcam is opened correctly
+	if not cap.isOpened():
+		raise IOError("Cannot open webcam")
+
+	# keep looping over the frames
+	while True:
+		ret, frame = cap.read()
+		frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+		
+		# detect the barcode in the image
+		box = detect(frame)
+		
+		# if a barcode was found, draw a bounding box on the frame
+		if box is not None:
+			cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+	 
+		# show the frame and record if the user presses a key
+		cv2.imshow("Frame", frame)
+		
+		c = cv2.waitKey(0)
+		if c == 27:
+			break
+	 
+	cap.release()
+	cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	main()
+
+
